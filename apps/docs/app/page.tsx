3512193a279afe7d5edc451af5d4a17ddb9f9c4a
Roy Jeong <geni429@gmail.com>
2023-08-10 15:49:10 +0900

    add door app

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,10 +1,19 @@
 import { Button, Header } from "ui";
+import Image from "next/image";
 
 export default function Page() {
   return (
     <>
       <Header text="Docs" />
       <Button />
+      <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        className="dark:invert"
+        width={100}
+        height={24}
+        priority
+      />
     </>
   );
 }
