2d394a96ab8c837667f747656eb8ca02e7013f76
Roy Jeong <geni429@gmail.com>
2023-08-10 16:45:43 +0900

    update next config

--- apps/door/next.config.js
+++ apps/door/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
 
-module.exports = nextConfig;
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  transpilePackages: ["ui"],
+};
