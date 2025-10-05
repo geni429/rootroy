5f44837a879fcbd3d4a9da58279b81aa21bca66c
geni429 <geni429@gmail.com>
2025-10-06 00:23:49 +0900

    create really simple clock ui

--- src/components/Watch/Watch.tsx
+++ src/components/Watch/Watch.tsx
@@ -2,6 +2,7 @@
 
 import useWatch from "./useWatch";
 import useIsClient from "@/hooks/useIsClient";
+import { css } from "../../../styled-system/css";
 
 export default function Watch() {
   const currentDate = useWatch();
@@ -10,5 +11,34 @@ export default function Watch() {
   if (!isClient) {
     return <div>--:--:--</div>;
   }
-  return <div>{currentDate.toISOString()}</div>;
+  return (
+    <>
+      <div>{currentDate.toISOString()}</div>
+      <div
+        className={css({
+          width: 500,
+          height: 500,
+          borderRadius: "50%",
+          position: "relative",
+          background: "black",
+        })}
+      >
+        <div
+          className={css({
+            width: 250,
+            height: 2,
+            background: "red",
+            transformOrigin: "0 0",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            translateY: "-50%",
+          })}
+          style={{
+            rotate: `${(currentDate.getSeconds() / 60) * 360 - 90}deg`,
+          }}
+        />
+      </div>
+    </>
+  );
 }
